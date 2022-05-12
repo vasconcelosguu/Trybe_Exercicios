f923d76a756030c20f0465862124678b6987b118
vasconcelosguu <vasconcelosgu604@gmail.com>
2022-05-12 15:29:01 -0300

    :art: Exercicios 1-4

---
+++ fundamentos/Bloco-4-JavaScript-Logica/dia-1-JavaScript/Exercicio-1/script.js
@@ -0,0 +1,9 @@
+const myName = "Gustavo";
+const birthCity = "Borborema";
+let birthYear = 2004;
+
+console.log (myName);
+
+console.log (birthCity);
+
+console.log (birthYear);
---
+++ fundamentos/Bloco-4-JavaScript-Logica/dia-1-JavaScript/Exercicio-4/script.js
@@ -0,0 +1,20 @@
+let weekDay = "sabádo";
+
+if (weekDay === "segunda-feira") {
+  console.log("Oba, mais um dia de aprendizado na TRYBE >:D");
+} 
+else if (weekDay === "terça-feira") {
+  console.log("Oba, mais um dia de aprendizado na TRYBE >:D");
+} 
+else if (weekDay === "quarta-feira") {
+  console.log("Oba, mais um dia de aprendizado na TRYBE >:D");
+} 
+else if (weekDay === "quinta-feira") {
+console.log("Oba, mais um dia de aprendizado na TRYBE >:D");
+} 
+else if (weekDay === "sexta-feira") {
+  console.log("Oba, mais um dia de aprendizado na TRYBE >:D");
+} 
+else {
+  console.log("FINALMENTE, descanso merecido UwU");
+}
