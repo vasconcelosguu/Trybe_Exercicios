4869c8fefb62bdb52898ffb707cc95cedb612c86
vasconcelosguu <vasconcelosgu604@gmail.com>
2022-05-13 13:53:57 -0300

    :zap: Exercicios For e Array

---
+++ logica/script.js
@@ -0,0 +1,16 @@
+//3. Lanchonete da Trybe
+
+//A Trybe abriu uma lanchonete e precisa criar um menu de opções para que as pessoas consigam
+// escolher qual lanche maravilhoso irão pedir.
+
+//Para isso escreva um algoritmo que recebe o número da opção escolhida conforme a tabela abaixo 
+ //e imprima no terminal a mensagem de acordo com a opção escolhida.
+
+//Tabela:
+//"1 - Trybe Lanche Feliz"
+//"2 - McTrybe"
+//"3 - TrybeWooper"
+//"4 - X-Trybe"
+//"5 - Triplo Trybe com JS"
+
+//Caso a pessoa escolha uma opção inexistente, imprima no terminal:
