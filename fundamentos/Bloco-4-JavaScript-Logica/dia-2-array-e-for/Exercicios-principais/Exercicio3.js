10d22aac9a131c17ab287c19a0cc1f3bd9d7b2b4
vasconcelosguu <vasconcelosgu604@gmail.com>
2022-05-13 15:49:14 -0300

    :zap: Todos os exercicios array/for

--- fundamentos/Bloco-4-JavaScript-Logica/dia-2-array-e-for/Exercicios-principais/Exercicio3.js
+++ fundamentos/Bloco-4-JavaScript-Logica/dia-2-array-e-for/Exercicios-principais/Exercicio3.js
@@ -1,10 +1,10 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
-let divide = 0;
 
-for (let index = 0; index < numbers.length; index +=1) {
+
+for (let index = 0; index < numbers.length; index ++) {
   soma += numbers[index];
-  divide = soma / numbers.length
 }
 
-console.log(divide);
+const media = soma / numbers.length;
+console.log(media); 
--- fundamentos/Bloco-4-JavaScript-Logica/dia-2-array-e-for/Exercicios-principais/Exercicio5.js
+++ fundamentos/Bloco-4-JavaScript-Logica/dia-2-array-e-for/Exercicios-principais/Exercicio5.js
@@ -2,5 +2,8 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let maiorNumero = 0;
 
 for (let i = 0; i < numbers.length; i++) {
-  
+  if (numbers[i] > maiorNumero) {
+    maiorNumero = numbers[i];
+  }
 }
+  console.log(maiorNumero);
