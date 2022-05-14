560a185d3f3d9c22a6e3323ff9f4f6070ed7d316
vasconcelosguu <vasconcelosgu604@gmail.com>
2022-05-14 17:49:32 -0300

    :zap: Exercicio bonus

---
+++ fundamentos/Bloco-4-JavaScript-Logica/dia-2-array-e-for/Bônus/script.js
@@ -0,0 +1,48 @@
+// BÔNUS 2 //
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// for (let i = 1; i < numbers.length; i++) {
+//   for (let secondI = 0; secondI < i; secondI++) {
+//     if (numbers[i] < numbers[secondI]) {
+//       let position = numbers[i];
+//       numbers[i] = numbers[secondI];
+//       numbers[secondI] = position;
+//     }
+//   }
+// }
+// console.log(numbers);
+
+
+//  BÔNUS 2 //
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// for (let i = 1; i < numbers.length; i++) {
+//   for (let secondI = 0; secondI < i; secondI++) {
+//     if (numbers[i] > numbers[secondI]) {
+//       let position = numbers[i];
+//       numbers[i] = numbers[secondI];
+//       numbers[secondI] = position;
+//     }
+//   }
+// }
+// console.log(numbers);
+//
+//
+//
+//
+// BÔNUS 3 //
+//
+// const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];;
+// const newNumbers = [];
+
+
+// for (let i = 0; i < numbers.length; i++) {
+//   if (i < numbers.length -1) {
+//     newNumbers.push(numbers[i] * numbers[i + 1]);
+//   } else {
+//     newNumbers.push(numbers[i] *2);
+//   }
+// }
+
+// console.log(newNumbers);
