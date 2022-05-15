af9745ef271f1faf62a5b0b0a226d4000432d327
vasconcelosguu <vasconcelosgu604@gmail.com>
2022-05-15 17:01:07 -0300

    :zap: exercicios 4.3

---
+++ fundamentos/Bloco-4-JavaScript-Logica/dia-3-logica-de-programacao/Exercicio-1.js
@@ -0,0 +1,38 @@
+// let number = 10;
+// let fatorial = number;
+
+
+// for (let i = number -1; i > 1; i--) {
+//   fatorial *= i;
+// }
+//   console.log(fatorial);
+
+
+//exercicio 2 //
+
+// let word = "ate cubanos";
+// let invert ="";
+
+// for (let i = word.length - 1; i >= 0; i--) {
+//   invert += word[i];
+// }
+
+
+// console.log(invert);
+
+
+//Exercicio 3 //
+
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+// let maiorPalavra = array[0];
+// let menorPalavra = array[0];
+
+// for (let i = 1; i < array.length; i++) {
+//   if (array[i].length > maiorPalavra.length) {
+//     maiorPalavra = array[i];
+//   } else if (array[i].length < menorPalavra.length) {
+//     menorPalavra = array[i];
+//   };
+// };
+
+// console.log(menorPalavra, maiorPalavra);
