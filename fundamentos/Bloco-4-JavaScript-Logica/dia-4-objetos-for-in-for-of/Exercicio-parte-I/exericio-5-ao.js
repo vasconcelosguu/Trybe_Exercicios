9c8797fab24c2d1e045a0f4addfbd0d90d0fd64c
vasconcelosguu <vasconcelosgu604@gmail.com>
2022-05-18 02:19:23 -0300

    :zap: Termino dos exercicios

--- fundamentos/Bloco-4-JavaScript-Logica/dia-4-objetos-for-in-for-of/Exercicio-parte-I/exericio-5-ao.js
+++ fundamentos/Bloco-4-JavaScript-Logica/dia-4-objetos-for-in-for-of/Exercicio-parte-I/exericio-5-ao.js
@@ -1,30 +1,30 @@
-//Exercicio 5
+// //Exercicio 5
 
-let info = {
-  personagem: 'Margarida',
-  origem: 'Pato Donald',
-  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  recorrente: 'Sim',
-};
+// let info = {
+//   personagem: 'Margarida',
+//   origem: 'Pato Donald',
+//   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   recorrente: 'Sim',
+// };
 
-let info2 = {
-  personagem: 'Tio Patinhas',
-  origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
-  nota: 'O último MacPatinhas',
-  recorrente: 'Sim',
-};
+// let info2 = {
+//   personagem: 'Tio Patinhas',
+//   origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
+//   nota: 'O último MacPatinhas',
+//   recorrente: 'Sim',
+// };
 
-for (let values in info) {
-  if (
-    values === 'recorrente' &&
-    info[values] === 'Sim' &&
-    info2[values] === 'Sim'
-  ) {
-    console.log('Ambos recorrentes');
-  } else {
-    console.log(info[values] + ' e ' + info2[values]);
-  }
-}
+// for (let values in info) {
+//   if (
+//     values === 'recorrente' &&
+//     info[values] === 'Sim' &&
+//     info2[values] === 'Sim'
+//   ) {
+//     console.log('Ambos recorrentes');
+//   } else {
+//     console.log(info[values] + ' e ' + info2[values]);
+//   }
+// }
 
 
 //Exercicio 6
@@ -42,7 +42,7 @@ let leitor = {
   ],
 };
 
-console.log("O livro favorito de " + leitor["nome"] + " " + leitor["sobrenome"]+ " se chama " + leitor.livrosFavoritos[0].titulo);
+// console.log("O livro favorito de " + leitor["nome"] + " " + leitor["sobrenome"]+ " se chama " + leitor.livrosFavoritos[0].titulo);
 
 
 //Exercicio 7
@@ -54,5 +54,12 @@ leitor.livrosFavoritos.push(
     editor: 'Rocco',
   },
 );
-console.log(leitor.livrosFavoritos)
+// console.log(leitor.livrosFavoritos)
 
+//Exercicio 8🚀 - Acesse as chaves nome e livrosFavoritos e faça um console.log no seguinte formato: "Julia tem 2 livros favoritos".
+let favBook
+
+if (leitor.livrosFavoritos = 2) {
+  favBook = "2 livros Favoritos";
+};
+  console.log(leitor.nome + " tem " + favBook);
