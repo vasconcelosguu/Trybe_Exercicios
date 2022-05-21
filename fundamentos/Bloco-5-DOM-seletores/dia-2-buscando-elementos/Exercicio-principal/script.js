c28fe30539da026eb80e7cf4544aae68619e9153
vasconcelosguu <vasconcelosgu604@gmail.com>
2022-05-20 21:37:21 -0300

    :zap: Exercicios 3 e 4

--- fundamentos/Bloco-5-DOM-seletores/dia-2-buscando-elementos/Exercicio-principal/script.js
+++ fundamentos/Bloco-5-DOM-seletores/dia-2-buscando-elementos/Exercicio-principal/script.js
@@ -11,6 +11,19 @@ body.appendChild(newH1);
 let newMain = document.createElement('main')
 
 newMain.className = 'main-content';
-
 body.appendChild(newMain);
 console.log(newMain);
+
+//Exercicio 3
+const main = document.querySelector('main');
+let newSection = document.createElement('section');
+
+newSection.className = 'center-content';
+main.appendChild(newSection);
+
+//Exercicio 4
+const section = document.querySelector('section');
+let newP = document.createElement('p');
+
+newP.innerText = 'Eu amo lontras';
+section.appendChild(newP);
